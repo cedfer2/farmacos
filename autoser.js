58adbb12b824838a35fe4e6f4a2bcd3e8aa3a941
cedfer2 <cedfer2@users.noreply.github.com>
2020-09-24 21:36:02 -0500

    zzzz
    zzz

--- autoser.js
+++ autoser.js
@@ -1,4 +1,5 @@
 (function($){
+	$(document).ready(function(){
 	var url = "https://cedfer2.github.io/farmacos/";
 	var img = {0:["a1", "a2", "a3","a4"], 1:["b1", "b2", "b3","b4"], 2:["c1", "c2", "c3","c4"]};
 	$('.widget-introduction-introduction-1 [data-ux="GridCell"]:first-child', window.parent.document).empty();
@@ -11,7 +12,7 @@
 			$('.ImgContPa'+y, window.parent.document).each(function(index,elemento){
 				$(elemento).append(function(){
 					return ($.map(img[y], function(ell,ind) {
-						return '<div class=ImgContainer data-ux=GridCell data-aid=GALLERY_IMAGE'+y+'_CELL_RENDERED><img src="'+url+ell+'.svg" alt=image'+ind+'data-ux=Image data-aid=GALLERY_IMAGE'+ind+'_RENDERED class="x-el x-el-img c2-1 c2-2 c2-1w c2-1h c2-2b c2-2c c2-1q c2-1s c2-18 c2-f c2-2d c2-3 c2-4 c2-5 c2-6 c2-7 c2-8"></div>';
+						'<div class=ImgContainer data-ux=GridCell data-aid=GALLERY_IMAGE'+y+'_CELL_RENDERED><img src="'+url+ell+'.svg" alt=image'+ind+'data-ux=Image data-aid=GALLERY_IMAGE'+ind+'_RENDERED class="x-el x-el-img c2-1 c2-2 c2-1w c2-1h c2-2b c2-2c c2-1q c2-1s c2-18 c2-f c2-2d c2-3 c2-4 c2-5 c2-6 c2-7 c2-8"></div>';
 						})).join(' ');
 				});
 			});
@@ -21,4 +22,5 @@
 				console.log(e);
 			});
 		});
+	});
 })(jQuery);
