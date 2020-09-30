eed5a1b3d022e8ed5233cee74fa94cbae988234d
cedfer2 <cedfer2@users.noreply.github.com>
2020-09-29 21:29:37 -0500

    Create contanctotop.js

---
+++ contanctotop.js
@@ -0,0 +1,7 @@
+(function($){
+	$(document).ready(function() {
+		$('.widget-header-header-9 .x-el-nav a[href*="facebook.com/"',window.parent.document).text('');
+		$( ".widget-contact-contact-9", window.parent.document).wrapAll("<div class='new' />");
+		$('.widget-content-content-7 [data-ux="Block"] [data-aid="CONTENT_IMAGE1_RENDERED"]',window.parent.document).removeAttr('srcset').attr('src', 'https://cedfer2.github.io/farmacos/cont.svg'); $(".widget-html-html-1 iframe", window.parent.document).before('<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/animate.min.css"/><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/helbeticaBQ.css"/><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto&family=Bree+Serif&display=swap"><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/contactanos.min.css"/><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/foter.min.css"/>');
+	});
+})(jQuery);
