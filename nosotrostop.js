d02ff4bb3dcba20c5f2b06ec504c559325ba14df
cedfer2 <cedfer2@users.noreply.github.com>
2020-09-29 21:16:49 -0500

    Create nosotrostop.js

---
+++ nosotrostop.js
@@ -0,0 +1,11 @@
+(function($){
+$('.widget-header-header-9 .x-el-nav a[href*="facebook.com/"',window.parent.document).text('');
+$(".widget-html-html-1 iframe", window.parent.document).before('
+	<link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/helbeticaBQ.css"/>
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css2?family=Roboto&family=Bree+Serif&display=swap">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/animate.min.css"/>
+	<link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/foter.min.css"/>
+	<link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/nosotros1.min.css"/>
+	');
+})(jQuery);
