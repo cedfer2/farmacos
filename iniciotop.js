848d3f796729bffddad473c230184cffd8ebeeb8
cedfer2 <cedfer2@users.noreply.github.com>
2020-10-01 16:52:53 -0500

    Update iniciotop.js

--- iniciotop.js
+++ iniciotop.js
@@ -2,4 +2,5 @@
 	$('.widget-header-header-9 .x-el-nav a[href*="facebook.com/"]:first-child',window.parent.document).text('');
 	$(".widget-html-html-1 iframe", window.parent.document).before('<link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/helbeticaBQ.css"/><link href="https://fonts.googleapis.com/css2?family=Roboto&family=Bree+Serif&display=swap" rel="stylesheet"><link rel="stylesheet" type=text/css href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/animate.min.css"/><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/foter.min.css"/><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/inicio.min.css"/>');
 	$('[data-aid="HAMBURGER_MENU_LINK"]', window.parent.document).click(function(){$('[data-ux="NavigationDrawer"]', window.parent.document).toggleClass('Sandshow');});
+	$('[data-ux="CloseIcon"]', window.parent.document).click(function(){$('[data-ux="NavigationDrawer"]', window.parent.document).removeClass('Sandshow');});
 })(jQuery);
