832e7171538471d713ef5a7705cfba58d2b47104
cedfer2 <cedfer2@users.noreply.github.com>
2020-12-04 00:36:23 -0600

    check bug apped
    view bug js

--- cooncc.js
+++ cooncc.js
@@ -5,17 +5,5 @@
 		$('.widget-header [data-ux="CloseIcon"]', window.parent.document).click(function(){$('[data-ux="NavigationDrawer"]', window.parent.document).removeClass('Sandshow');});
 		$( ".widget-contact-contact-9", window.parent.document).wrapAll("<div class='new' />");
 		$('.widget-content-content-7 [data-ux="Block"] [data-aid="CONTENT_IMAGE1_RENDERED"]',window.parent.document).removeAttr('srcset').attr('src', 'https://cedfer2.github.io/farmacos/cont.svg'); $(".widget-html-html-1 iframe", window.parent.document).before('<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/animate.min.css"/><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/helbeticaBQ.css"/><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto&family=Bree+Serif&display=swap"><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/contactanos.min.css"/><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/foter.min.css"/>');
-		if ($('.widget-contact-contact-9:first-Child .telCentro', window.parent.document).length<1)
-		{
-			$('.widget-contact-contact-9 [data-aid="CONTACT_INFO_PHONE_REND"]', window.parent.document).parent('[data-typography="BodyAlpha"]').addClass('telCentro');
-			$('.widget-contact-contact-9:last-Child [data-aid=CONTACT_INFO_ADDRESS_REND]+[data-ux=ContentText]', window.parent.document).addClass('innerTel');
-			$('.widget-contact-contact-9 [data-aid="CONTACT_HOURS_TITLE_REND"]', window.parent.document).parent('[data-ux="Block"]').addClass('RelogCentro');
-			$('.widget-contact-contact-9 [data-aid="CONTACT_INTRO_DESC_REND"]', window.parent.document).addClass('whatsCentro');
-			$('.widget-contact-contact-9 .innerTel', window.parent.document).append($('#6bda68c0-32f3-4be1-af1c-fdd6e1c39d82 [data-aid="CONTENT_WELCOME_DESCRIPTION_RENDERED"]', window.parent.document));
-			$('.widget-contact-contact-9:first-Child .telCentro', window.parent.document).after($('.widget-contact-contact-9:first-Child .RelogCentro', window.parent.document));
-			$('.widget-contact-contact-9:last-Child .telCentro', window.parent.document).after($('.widget-contact-contact-9:last-Child .RelogCentro', window.parent.document));
-			$('.widget-contact-contact-9:last-Child .RelogCentro', window.parent.document).after($('.widget-contact-contact-9:last-Child .whatsCentro', window.parent.document));
-			$('#6bda68c0-32f3-4be1-af1c-fdd6e1c39d82', window.parent.document).remove();
-		}
 	});
 })(jQuery);
