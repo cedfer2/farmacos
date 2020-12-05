3ad646a8b9859f6f11754cdc54b05d6c6fac0cee
cedfer2 <cedfer2@users.noreply.github.com>
2020-12-04 19:20:55 -0600

    Update autoser.js

--- autoser.js
+++ autoser.js
@@ -22,7 +22,7 @@ modalImage = {
 	
 if ($('.polyfi', window.parent.document).length<1)
 {
-	$('head', window.parent.document).prepend('<script crossorigin="anonymous" src="https://polyfill.io/v3/polyfill.js"></script>"></script>');
+	$('head', window.parent.document).prepend('<script crossorigin="anonymous" src="https://polyfill.io/v3/polyfill.js"></script></script>');
 }
 	
 $('.widget-header-header-9 .x-el-nav a[href*="facebook.com/"]:first-child',window.parent.document).text('');
