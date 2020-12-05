5f2dc8f748efadacc3dfb2a9bdc05b938fba0ec7
cedfer2 <cedfer2@users.noreply.github.com>
2020-12-04 20:26:54 -0600

    vainillaPolyfilljs
    vainnnjsPolyyycccddddjs

--- fixes.js
+++ fixes.js
@@ -1,9 +1,11 @@
+var script = document.createElement("script");
+script.type="text/javascript";
+script.crossorigin="anonymous";
+script.src="https://polyfill.io/v3/polyfill.js";
+document.getElementsByTagName('head')[0].prepend(script);
+
 (function($){
 	$(document).ready(function() {
-		if ($('.polyfi', window.parent.document).length<1)
-		{
-			$('head', window.parent.document).prepend('<script crossorigin="anonymous" src="https://polyfill.io/v3/polyfill.js"></script></script>');
-		}
 		$('.widget-header-header-9 .x-el-nav a[href*="facebook.com/"]:first-child',window.parent.document).text('');
 		$('.widget-header [data-aid="HAMBURGER_MENU_LINK"]', window.parent.document).click(function(){$('[data-ux="NavigationDrawer"]', window.parent.document).toggleClass('Sandshow');});
 		$('.widget-header [data-ux="CloseIcon"]', window.parent.document).click(function(){$('[data-ux="NavigationDrawer"]', window.parent.document).removeClass('Sandshow');});
