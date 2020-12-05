c09522df0d420f18244bd560ad1ee6a9a1973a3a
cedfer2 <cedfer2@users.noreply.github.com>
2020-12-05 05:44:55 -0600

    Update orientacion.js

--- orientacion.js
+++ orientacion.js
@@ -1,10 +1,14 @@
 (function($){
 	$(document).ready(function() {
-		$('.widget-header-header-9 .x-el-nav a[href*="facebook.com/"]:first-child',window.parent.document).text('');
-		$('.widget-header [data-aid="HAMBURGER_MENU_LINK"]', window.parent.document).click(function(){$('[data-ux="NavigationDrawer"]', window.parent.document).toggleClass('Sandshow');});
-		$('.widget-header [data-ux="CloseIcon"]', window.parent.document).click(function(){$('[data-ux="NavigationDrawer"]', window.parent.document).removeClass('Sandshow');});
 		if ($('.newGrup', window.parent.document).length<1){
-		$( ".widget-content-content-9", window.parent.document).wrapAll("<div class='newGrup' />");
+			$( ".widget-content-content-9", window.parent.document).wrapAll("<div class='newGrup' />");
+			$.each($('.widget-content-content-9 [data-ux="ContentCards"] [data-ux="GridCell"]:not(:first-child) > [data-ux="ContentCard"]',window.parent.document), function(index, el) {
+				if ($(el).children('[data-ux="ContentCardWrapperImage"]').length>0)
+				{
+					$(el).addClass('row v-center reverse thumnn');
+					$(el).children('[data-ux="ContentCardText"], [data-ux="Block"]').wrapAll('<div class="extra-wrapper"></div>');
+				}
+			});
 		}
 		$(".widget-html-html-1 iframe", window.parent.document).before('<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/animate.min.css"/><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/helbeticaBQ.css"/><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto&family=Bree+Serif&display=swap"><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/orientacion.min.css"/><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/foter.min.css"/>');
 	});
