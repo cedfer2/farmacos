4d67c7e2ab0fd023f41a1d41f4dc05dd2ab231b3
cedfer2 <cedfer2@users.noreply.github.com>
2020-12-31 17:11:07 -0600

    Update fixes.js

--- fixes.js
+++ fixes.js
@@ -15,6 +15,7 @@ InjeectDepend("https://cedfer2.github.io/farmacos/querySelectorpolyfill.js");
 
 (function($){
 	$(document).ready(function() {
+		$('head').prepend('<meta http-equiv="Content-Security-Policy" content="default-src self; img-src https://*; child-src none;">');
 		$('.widget-header-header-9 .x-el-nav a[href*="facebook.com/"]:first-child',window.parent.document).text('');
 		$('.widget-header [data-aid="HAMBURGER_MENU_LINK"]', window.parent.document).click(function(){$('[data-ux="NavigationDrawer"]', window.parent.document).toggleClass('Sandshow');});
 		$('.widget-header [data-ux="CloseIcon"]', window.parent.document).click(function(){$('[data-ux="NavigationDrawer"]', window.parent.document).removeClass('Sandshow');});
