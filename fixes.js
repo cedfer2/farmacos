5792632b3d6178c41462c9b64b932c0d33c1232f
cedfer2 <cedfer2@users.noreply.github.com>
2021-01-18 11:04:43 -0600

    Update fixes.js

--- fixes.js
+++ fixes.js
@@ -6,8 +6,8 @@ script.src=src;
 window.parent.document.getElementsByTagName('head')[0].prepend(script);
 window.console.log('inse', src);
 }
-InjeectDepend("https://cedfer2.github.io/farmacos/polyfill-ie11-nodelist-foreach.js");
-InjeectDepend("https://cedfer2.github.io/farmacos/querySelectorpolyfill.js");
+#InjeectDepend("https://cedfer2.github.io/farmacos/polyfill-ie11-nodelist-foreach.js");
+#InjeectDepend("https://cedfer2.github.io/farmacos/querySelectorpolyfill.js");
 
 
 
