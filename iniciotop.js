a23ad9ead1857d799b56360ee2c8ec7a7fa55b29
cedfer2 <cedfer2@users.noreply.github.com>
2021-01-21 17:08:32 -0600

    Update iniciotop.js

--- iniciotop.js
+++ iniciotop.js
@@ -1,5 +1,5 @@
 (function($){
-  $(window).load(function() {
+  $(window.parent.document).ready(function() {
     $("head", window.parent.document).append('<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css2?family=Roboto&family=Bree+Serif&display=swap"><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/slick.css"><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/slick-theme.css"><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/fuentes/font-awesome.min.css"><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/animate.min.css"><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/foter.min.css"><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/inicio.min.css">');
     var url = "https://cedfer2.github.io/farmacos/inicio/";
     var items = ['banner1.webp','banner2.jpg','banner4.jpg'];
