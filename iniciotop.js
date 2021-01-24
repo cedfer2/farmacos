b9b7eb0255f07c65d5c564e02d86ff7fb1515056
cedfer2 <cedfer2@users.noreply.github.com>
2021-01-23 21:47:49 -0600

    Update iniciotop.js

--- iniciotop.js
+++ iniciotop.js
@@ -1,5 +1,4 @@
 (function($){
-  $(document).ready(function(){
     var url = "https://cedfer2.github.io/farmacos/inicio/";
     var items = ['banner1.webp','banner2.jpg','banner4.jpg'];
     $("head", window.parent.document).append('<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css2?family=Roboto&family=Bree+Serif&display=swap"><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/slick.css"><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/slick-theme.css"><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/fuentes/font-awesome.min.css"><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/animate.min.css"><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/foter.min.css"><link rel="stylesheet" type="text/css" href="https://cedfer2.github.io/farmacos/inicio.min.css">');
@@ -18,5 +17,5 @@
       $('.js-slider', window.parent.document).not('.slick-initialized').slick('resize');
     });
   }else{ console.log('slick: err'); }
-});
+  alert('change scripts');
 })(jQuery);
